69fb8409811bdce72e12d78dad7a36ea51f3018e
cristian-cuero <cuero.stiven97@gmail.com>
2025-05-05 15:56:00 -0500

    Correcion Asesores -Departamentos

--- database/queries/asesores.js
+++ database/queries/asesores.js
@@ -7,7 +7,7 @@ const loadAsesores = async (TIPO) => {
   return new Promise((resolve, reject) => {
     pool.get((err, db) => {
       if (err) return reject(err);
-      const sql = `SELECT null id , idpersona  id_persona ,  CAST(p.NOMBRES AS VARCHAR(30) CHARACTER SET WIN1252) nombre1, '' nombre2 , CAST(p.APELLIDOS AS VARCHAR(30) CHARACTER SET WIN1252) as apellido1 , '' apellido2, p.telefono telefono1 , '' telefono2 , 0 estado, p.tipo  tipo_Empleado, p.parentesco email, 1 enviarcopiacto, null cod_Respuesta,  null msj_Respuesta, null subdominio FROM TBLPERSONA p WHERE p.TIPO= ? AND p.ZIPCONTRATANTE='ACTIVO'`;
+      const sql = `SELECT null id , idpersona  id_persona ,  CAST(p.NOMBRES AS VARCHAR(30) CHARACTER SET WIN1252) nombre1, '' nombre2 , CAST(p.APELLIDOS AS VARCHAR(30) CHARACTER SET WIN1252) as apellido1 , '' apellido2, p.telefono telefono1 , '' telefono2 , 1 estado, p.tipo  tipo_Empleado, p.parentesco email, 1 enviarcopiacto, null cod_Respuesta,  null msj_Respuesta, null subdominio FROM TBLPERSONA p WHERE p.TIPO= ? AND p.ZIPCONTRATANTE='ACTIVO'`;
       db.query(sql, [TIPO], (err, result) => {
         db.detach();
         const converted = result.map((row) =>
